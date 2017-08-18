3623cb06356b526a7d1a575f2ffb4184ad42632f
Bana Abraham <bana.abraham@gmail.com>
2017-08-18 09:06:59 +0700

    Create google_price.py

---
+++ google_price.py
@@ -0,0 +1,37 @@
+import urllib
+from yahoo_finance import Share
+import re
+
+class google_read(object):
+    def __init__(self,stock):
+        self.stock = stock
+        
+    def get_text(self):
+        url = 'https://www.google.com/finance?q=IDX:'+self.stock
+        hdr = {'User-Agent': 'Mozilla/5.0'}
+        print (url)
+        response = urllib.request.Request(url,headers=hdr)                                
+        with urllib.request.urlopen(response) as f:
+            self.page = f.read().decode('utf-8')
+        
+        return self.page.split('\n')   
+    
+    def read_page(self):
+        #f = open(self.page,'r')
+        f = self.page.split('\n')
+        trigger = 0
+        file = []
+        #text = [x for x in f.readlines()]
+        text = f
+        for i in range(len(text)):
+            if '<span class="pr">' in text[i]:
+                trigger=1
+            if trigger==1:
+                file.append(text[i])
+        c = re.findall(">.*<",str(file[1]))  
+        if c:
+            return float(str(c[0]).replace("<","").replace(">","").replace(",",""))
+        
+s = google_read('aapl')   
+s.get_text()
+page = s.read_page()
