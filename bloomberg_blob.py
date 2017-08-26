26253a60b99d2e18d340136d488d96fa5af0c0dd
Bana Abraham <bana.abraham@gmail.com>
2017-08-26 08:48:33 +0700

    Create bloomberg_blob.py
    analyze news sentiment on bloomberg

---
+++ bloomberg_blob.py
@@ -0,0 +1,50 @@
+import urllib
+import re
+import pandas as pd
+from textblob import TextBlob
+class bloomberg(object):
+    def __init__(self,query):
+        self.query = query
+        
+    def get_text(self):
+        url = 'https://www.bloomberg.com/search?query='+self.query
+        hdr = {'User-Agent': 'Mozilla/5.0'}
+        #print (url)
+        response = urllib.request.Request(url,headers=hdr)                                
+        with urllib.request.urlopen(response) as f:
+            self.page = f.read().decode('utf-8')
+        
+        return self.page.split('\n')
+    
+    def get_info(self):
+        page = self.page.split('\n')
+        for i in range(len(page)):
+            if "storyType" in page[i]:
+                pertinent = page[i]
+        info = pertinent.split("storyType")
+        self.news = []
+        
+        for i in info:
+            self.news.append(str(re.findall('"body":.*"publishedAt"',i)).replace("body","").replace("publishedAt",""))
+        self.newsclean = []
+        for i in self.news:
+            self.newsclean.append((re.sub(r'\W+', ' ', i)))           
+        return self.newsclean        
+    def analyze(self):
+        for i in self.newsclean:
+            null = 0
+            positive = 0        
+            blob = TextBlob(i).sentiment
+            if blob.subjectivity == 0:
+                null+=1
+            if blob.polarity>0:
+                print(i)
+                positive+=1           
+                sentiment=positive/(len(self.newsclean))
+        return sentiment*100 
+                        
+        
+goog = bloomberg("exxon")
+page = goog.get_text()
+info = goog.get_info()    
+sentiment = goog.analyze()
